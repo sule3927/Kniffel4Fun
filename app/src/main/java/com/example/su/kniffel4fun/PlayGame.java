e139e2e47fced1aa5dc371e9e8080612ca38ff27
MerleFriTH <merle.friedrichsen@th-wildau.de>
2018-04-08 14:37:18 +0200

    javadoc bei playgame hinzugefügt

--- app/src/main/java/com/example/su/kniffel4fun/PlayGame.java
+++ app/src/main/java/com/example/su/kniffel4fun/PlayGame.java
@@ -1,50 +1,82 @@
 package com.example.su.kniffel4fun;
 
 /**
- * Created by judith on 06.03.2018.
+ * @author Judith Balß on 06.03.2018.
  */
 
-
-import android.util.Log;
-
-
+/**
+ * the class PlayGame is the model,
+ * it includes an object currPlayer that points to the current Player,
+ * an object currTurn, that points to the current turn,
+ * and it counts the played rounds
+ */
 public class PlayGame {
     private static Player currPlayer;
     private static turn currTurn;
     private static int countRounds = 0;
 
+    /**
+     * constructor of a PlayGame,
+     * creates a new turn, set the newly created turn to currTurn,
+     * und sets the first Player from the arrayList allPlayers to the current Player
+     */
     public PlayGame() {
         turn turn = new turn();
         setCurrTurn(turn);
         setCurrPlayer(Player.allPlayers.get(0));
     }
 
+    /**
+     * method to get how many rounds have been player
+     * @return int, the number of played rounds
+     */
     public static int getCountRounds() {
         return countRounds;
     }
 
+    /**
+     * method to return the currently Player-Object
+     * @return Player who is playing
+     */
     public static Player getCurrPlayer() {
         return currPlayer;
     }
 
+    /**
+     * method to declare which is the current player
+     * @param currPlayer Player whos turn it shall be
+     */
     public static void setCurrPlayer(Player currPlayer) {
         PlayGame.currPlayer = currPlayer;
     }
 
+    /**
+     * method to get the current player
+     * @return Player whos turn it is
+     */
     public static turn getCurrTurn() {
         return currTurn;
     }
 
+    /**
+     * method to set the current turn
+     * @param currTurn
+     */
     public static void setCurrTurn(turn currTurn) {
         PlayGame.currTurn = currTurn;
     }
 
+    /**
+     * method to change the player,
+     * creates a new turn and sets it a current turn,
+     * sets the next player as current player,
+     * it the end of the arrayList allPlayers is reached, it switches to the first player and increments contRounds
+     */
     public static void changePlayer() {
         turn turn = new turn();
         setCurrTurn(turn);
         Player currPlayer = PlayGame.getCurrPlayer();
         int indexNext = 1 + Player.allPlayers.indexOf(currPlayer);
-        //wenn Ende des Array erreicht, fange von vorne an
         if (indexNext == Player.allPlayers.size()) {
             indexNext = 0;
             PlayGame.countRounds++;
@@ -53,19 +85,9 @@ public class PlayGame {
         PlayGame.setCurrPlayer(nextPlayer);
     }
 
-    /*this method creates two players for test purposes*/
-    public int testPlayer() {
-        Player player1 = new Player("Susann", 2131099732);
-        Player player2 = new Player("Judith", 2131099758);
-        Player player3 = new Player("Merle", 2131099758);
-        setCurrPlayer(Player.allPlayers.get(0));
-        //player1.setPoints(3, 0);
-        //player2.setPoints(4, 0);
-        //turn turn = new turn();
-        //setCurrTurn(turn);
-        return 12; /*dieser Returnwert wird nicht gebraucht, allerdings funktionierte die Methode nur, wenn ich einer Varaiblen den WErt zuweise*/
-    }
-
+    /**
+     * this method interates over allPlayers and calculates the results
+     */
     public static void nextResult(){
        int thisPlayer = 0;
        while ( thisPlayer <Player.allPlayers.size()) {
@@ -76,6 +98,9 @@ public class PlayGame {
         }
     }
 
+    /**
+     * this method calculates the end-result for the current Player
+     */
     public static void calculateResult(){
         int[] scores = currPlayer.scores;
         int sumTop = scores[0] + scores[1] + scores[2] + scores[3] + scores[4] + scores[5];
@@ -89,7 +114,5 @@ public class PlayGame {
         }
         currPlayer.finalScore[3] = scores[12];
         currPlayer.finalScore[4] = currPlayer.finalScore[0] + currPlayer.finalScore[1] + currPlayer.finalScore[2] + currPlayer.finalScore[3];
-        Log.d("SENSO", getCurrPlayer() + " oben" + currPlayer.finalScore[0] + " bonus " + currPlayer.finalScore[1] + " unten " + currPlayer.finalScore[2] + " Kniffel " + currPlayer.finalScore[3] + " Total " + currPlayer.finalScore[4]);
     }
-
 }
